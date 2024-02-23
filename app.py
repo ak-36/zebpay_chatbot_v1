d2e0992d6584db0c46b1d68b0af7ae72909c6190
ak-36 <86473252+ak-36@users.noreply.github.com>
2024-02-23 22:48:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,13 +76,16 @@ for message in st.session_state.messages:  # Display the prior chat messages
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = chat_engine.stream_chat(prompt)
+            response = chat_engine.chat(prompt)
             esc_input = f"User Query: {prompt}, Bot Response: {response}"
             esc_response = esc_engine.chat(esc_input)
             if str(esc_response).lower() == "true":
-                response_text = " ".join(token for token in response.response_gen) + "\nConnecting you with customer support"
+                # response_text = " ".join(token for token in response.response_gen) + "\nConnecting you with customer support"
+                response_text = str(response.response) + "\nConnecting you with customer support"
+                st.write(response_text)
             else:
-                response_text = " ".join(token for token in response.response_gen)
-            st.write(response_text)
-            message = {"role": "assistant", "content": response_text}
+                # response_text = " ".join(token for token in response.response_gen)
+                st.write(response)
+            # st.write(response_text)
+            message = {"role": "assistant", "content": response}
             st.session_state.messages.append(message)  # Add response to message history
